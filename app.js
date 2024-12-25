7f9399ba8108defff63713aa3f26185af79aab7f
Fahim047 <fahimulislam58@gmail.com>
2024-12-25 14:01:11 +0600

    ✅ Express and mongodb setup done

---
+++ app.js
@@ -0,0 +1,21 @@
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+	})
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.get('/', (req, res) =>
+	res.status(200).json({ message: 'Study together!!!' })
+);
+
+export { app };
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+import dotenv from 'dotenv';
+import { app } from './app.js';
+import { connectDB } from './db/db.js';
+dotenv.config({
+	path: './.env',
+});
+const port = process.env.PORT || 8000;
+
+connectDB()
+	.then(() => {
+		app.on('error', (err) => {
+			console.log('Error occured in the server: ', err);
+			throw err;
+		});
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	})
+	.catch((err) => {
+		console.error('MongoDB connection failed: ', err);
+	});
